7ff563ccf75dda38fed6799a314437afed52a51c
Milly <milly.@MacBook-Pro.local>
2021-01-16 16:42:21 -0500

    newfilez

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cv2
 import sys
+import numpy as np
+from keras.applications import resnet50
+from keras.models import load_model
 
 # pip install -r requirements.txt
 # sudo python main.py haarcascade_frontalface_default.xml
@@ -8,7 +11,9 @@ import sys
 
 def main():
     cascPath = sys.argv[1]
+    modelPath = sys.argv[2]
     faceCascade = cv2.CascadeClassifier(cascPath)
+    mask_classifier = load_model(modelPath)
 
     video_capture = cv2.VideoCapture(0)
 
@@ -16,19 +21,49 @@ def main():
         # Capture frame-by-frame
         ret, frame = video_capture.read()
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        masked_faces = []
+        unmasked_faces = []
 
-        faces = faceCascade.detectMultiScale(
+        # Detect Faces
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        detections = faceCascade.detectMultiScale(
             gray,
             scaleFactor=1.1,
             minNeighbors=5,
             minSize=(30, 30),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
+        print(detections.shape)
 
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        if detections.shape[0] > 0:
+            for i in range(detections.shape[0]):
+                # Get Co-ordinates
+                x, y, w, h = detections[i]
+                x1 = x
+                y1 = y
+                x2 = x + w
+                y2 = y + h
+
+
+                # Predict Output
+                face_arr = cv2.resize(frame[y1:y2,x1:x2,::-1], (224, 224), interpolation=cv2.INTER_NEAREST)
+                face_arr = np.expand_dims(face_arr, axis=0)
+                face_arr = resnet50.preprocess_input(face_arr)
+                match = mask_classifier.predict(face_arr)
+
+                if match[0][0]<0.5:
+                    masked_faces.append([x1,y1,x2,y2])
+                else:
+                    unmasked_faces.append([x1,y1,x2,y2])
+
+        # Put Bounding Box on the Faces (Green:Masked,Red:Not-Masked)
+        for f in range(len(masked_faces)):
+            a,b,c,d = masked_faces[f]
+            cv2.rectangle(frame, (a,b), (c,d), (0,255,0), 2)
+
+        for f in range(len(unmasked_faces)):
+            a,b,c,d = unmasked_faces[f]
+            cv2.rectangle(frame, (a,b), (c,d), (0,0,255), 2)
 
         # Display the resulting frame
         cv2.imshow('Video', frame)
