27db01b4e4ed0c400827a2386c0b40b4c0b144ff
Michael Vytlingam <michael.vytlingam@gmail.com>
2021-01-16 15:45:59 -0500

    Adds webcam functionality with simple face detection

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
+import cv2
+import sys
+
+# pip install -r requirements.txt
+# sudo python main.py haarcascade_frontalface_default.xml
+
+# may need to try running in different terminals to trigger camera permissions
+
 def main():
-    print('hello world')
+    cascPath = sys.argv[1]
+    faceCascade = cv2.CascadeClassifier(cascPath)
+
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
